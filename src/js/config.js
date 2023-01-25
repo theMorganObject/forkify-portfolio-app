049d4cafa460931c1086bb1e2faa2b5110ed9b5b
theMorganObject <morgan.oshaughnessey@gmail.com>
2023-01-24 18:28:26 -1000

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'a1070a59-e9a1-40d8-bfa6-0373e02b89ee';
+export const MODAL_CLOSE_SEC = 2.5;
